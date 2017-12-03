5f60afbc6ac2372b3a137259b846608120e25534
Justin Farrow <landsurveyorsunited@gmail.com>
2017-12-02 21:12:51 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 		$('.view').removeClass('filter-red_blur');
 	}, 500);
 	
-  var analysis = "^4000<h2>analysis:</h2>^1000<p>Survey Earth</p>^200<p>In a Day</p>^200<p>SEVEN</p>^200<p>WORLDWIDE</p><p>SUMMER SOLSTICE<br/><br/></p><p>2thousand18</p><p>LOG A POINT</p><p>at NOON your local time</p>";
+  var analysis = "^^4000<h2>💮experience:</h2>^1000<p>💮14 yrs Web development + social media</p>^200<p>📚Masters Global Sustainability Entrepreneurship | USF</p>^200<p>📚Masters Sustainable Tourism USF </p>^200<p>📚BS Anthropology // BA Philosophy & Religion | CofC</p><p>📍Cartography / Mapping / Geosocial Network Developer<br/><br/></p><p>10 years community development - Land Surveyors United</p><p>Creator / Administrator of Survey Earth in a Day Event</p><p>email:justin.farrow-g📧</p><p>current location:</p><p>Tampa< FL</p><p>set up interview</p>";
 	
 	var tracking = "^1000match <span class='square'>&#9632;</span>";
   
 	$(function(){
 		var typed = new Typed("#report", {
-			strings: ["^2000<p>......</p>", "^1000<p>LAND SURVEYORS ONLY</p>SUBMIT POINT<br/><br/>^1000<p><p>HELP REMEASURE<br/><br/></p>^500<p>ENTIRE SURFACE<br/><br/></p>^500<p>OF EARTH<br/>in a single day<br/><br/></p>^500<p>SEIAD7</p><p>LAND SURVEYORS UNITED</p><p>surveying Planet</p><p>EARTH, together</p>"],
+			strings: ["^2000<p>......</p>", "^1000<p>checking references💯</p>everyone raved💯<br/><br/>^1000<p><p>assessment complete🃏<br/><br/></p>^500<p>fit probability 99.2<br/><br/></p>^500<p>reset to aquisition<br/> qualified</p><p>when can he start</p><p>active status</p><p>set up interview</p>"],
 			showCursor:false,
 			loop: false
 		});	
